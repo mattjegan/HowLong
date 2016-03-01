2040500affebb3bcb8f0025671758ace3b695116
mattjegan <matthewj.egan@hotmail.com>
2016-03-01 15:17:11 +1100

    Added HowLong.py and basic class

---
+++ HowLong.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+__author__ = "Matthew Egan"
+
+from sys import argv
+
+class HowLong:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__": HowLong().run()
