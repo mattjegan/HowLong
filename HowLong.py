3a30701ad7ac1fe521fb36bae0a13773fea0f5eb
mattjegan <matthewj.egan@hotmail.com>
2016-03-01 15:40:21 +1100

    Added naive functionality and formatting

--- HowLong.py
+++ HowLong.py
@@ -2,13 +2,24 @@
 
 __author__ = "Matthew Egan"
 
+from datetime import timedelta
+from subprocess import Popen
 from sys import argv
+from time import time, sleep
 
 class HowLong:
     def __init__(self):
-        pass
+        self.command = " ".join(argv[1:])
 
     def run(self):
-        pass
+        print("Running", self.command)
+        process = Popen(argv[1:])
+        start_time = time()
+
+        while process.poll() is None:
+            sleep(1)
+            print(str(timedelta(seconds=int(time() - start_time))))
+
+        print("Finished", self.command)
 
 if __name__ == "__main__": HowLong().run()
