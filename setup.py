a496363cc27337ebbe3ebd54d9cccf61f440f236
mattjegan <matthewj.egan@hotmail.com>
2016-08-24 16:21:00 +1000

    Updated LICENSE, added reqs and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(author='Matthew Egan',
+      author_email='matthewj.egan@hotmai.com',
+      description='A simple timing utility for long running processes',
+      name='HowLong',
+      py_modules=[
+          'HowLong.HowLong',
+      ],
+      url='https://github.com/mattjegan/howlong',
+      version='0.0.1'
+)
