3cb307a3601c207095b45d3588a22888fb5736f1
adhaamehab <adham@floralytics.com>
2017-10-21 21:41:29 +0800

    add m command to list history and mc to clear it

--- HowLong/HowLong.py
+++ HowLong/HowLong.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 import argparse
 import logging
 import sys
+import os
 
 from datetime import timedelta
 from subprocess import Popen
-from time import time, sleep
+from time import time, sleep, ctime
 import psutil
 
 from colorama import init
@@ -58,6 +59,10 @@ class HowLong(object):
         parser.add_argument('command_args', metavar='cmd_args', type=str,
                             nargs=argparse.REMAINDER,
                             help='additional arguments for target command')
+        parser.add_argument('-m',
+                            help='list history of HowLong', action="store_true")
+        parser.add_argument('-mc', action="store_true",
+                            help='clear history of HowLong')
         parsed_args = parser.parse_args()
 
         self.timer_interval = parsed_args.i if parsed_args.i else 1
@@ -66,16 +71,40 @@ class HowLong(object):
         cmd_args = parsed_args.command_args if parsed_args.command_args else None
         assert command is not None or cmd_args is None, "Can't have no command but have command args, \
                              usage with command is: howlong [options] -c your_command your_options"
+       
+
         if parsed_args.p:
             self.pid = int(parsed_args.p[0])
             self.command = None
             assert command is None, "can't have both -p and -c"
             assert self.pid in psutil.pids(), "argument p must be a valid pid, %d is not one" % pid
         else:
-            assert command is not None, "you must use either -p or -c"
-            self.pid = None
-            cmd_args = [] if cmd_args is None else cmd_args
-            self.command = command + cmd_args
+
+            if parsed_args.m:
+                try : 
+                    with open('history.txt', 'r') as f:
+                        history = f.readlines()
+                        for line in history:
+                            print(line.strip())
+                            
+                except:
+                    print("\nNo history avaialable\n")
+                exit()
+            elif parsed_args.mc:
+                if os.path.isfile('history.txt'):
+                    try:
+                        os.remove('history.txt')
+                        print('History deleted!\n')
+                    except:
+                        print('Can\'t delete history!\n')
+                else:
+                    print('\nNo history found!')
+                exit()
+            else:
+                assert command is not None, "you must use either -p or -c"
+                self.pid = None
+                cmd_args = [] if cmd_args is None else cmd_args
+                self.command = command + cmd_args
         
         self.log_file = parsed_args.f[0] if parsed_args.f else None
         
@@ -98,11 +127,16 @@ class HowLong(object):
         readable_command = process.command
         start_time = process.start_time
         logging.debug(colored("Running " + readable_command), 'green')
+        with open('history.txt', 'a') as history: # append process starting time to history
+            history.write("pid #{}  started at  {}\n".format(self.pid, ctime(int(start_time))))
         while process.is_running():
             sleep(self.timer_interval)
             elapsed_time = (time() - start_time) * 1000
             logging.info(colored(str(timedelta(milliseconds=elapsed_time)), 'blue'))
         logging.debug(colored("Finished " + readable_command), 'red')
+        with open('history.txt', 'a') as history: # append process finishing time to history
+            history.write("pid #{}  finished at  {}\n".format(self.pid, ctime(int(time()))))
+        
 
 
 def howlong():
